10a33cf0f3fa48a02ce721a6b591aee219d13b0a
Gabriel Garcia <biel.garcia.mjv@gmail.com>
2024-03-01 13:17:51 -0300

    Create SetoresService.cs

---
+++ CrudWtithRenderModes/CrudWtithRenderModes.Client/Services/SetoresService.cs
@@ -0,0 +1,58 @@
+﻿using CrudWithRenderModes.Shared.Entities;
+using CrudWithRenderModes.Shared.Interfaces;
+using System.Net.Http.Json;
+using System.Reflection;
+using System.Text.Json;
+
+namespace CrudWtithRenderModes.Client.Services
+{
+    public class SetoresService : ISetoresRepository
+    {
+        private readonly HttpClient httpClient;
+        private readonly JsonSerializerOptions _options;
+
+        public SetoresService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            _options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+
+        public async Task<Setores> AddSetoresAsync(Setores model)
+        {
+            var setor = await httpClient.PostAsJsonAsync("api/Setores/Add-Setores", model);
+            var response = await setor.Content.ReadFromJsonAsync<Setores>();
+            return response!;
+        }
+
+        public async Task<Setores> DeleteSetoresAsync(int setoresId)
+        {
+            var setor = await httpClient.DeleteAsync($"api/Setores/Delete-Setores/{setoresId}");
+            var response = await setor.Content.ReadFromJsonAsync<Setores>();
+            return response!;
+        }
+
+        public async Task<List<Setores>> GetAllSetoresAsync()
+        {
+            var setor = await httpClient.GetAsync("api/Setores/Setores");
+            var response = await setor.Content.ReadFromJsonAsync<List<Setores>>();
+            return response!;
+        }
+
+        public async Task<Setores> GetSetoresByIdAsync(int setoresId)
+        {
+            var setor = await httpClient.GetAsync($"api/Setores/Setores/{setoresId}");
+            var response = await setor.Content.ReadFromJsonAsync<Setores>();
+            return response!;
+        }
+
+        public async Task<Setores> UpdateSetoresAsync(Setores model)
+        {
+            var setor = await httpClient.PutAsJsonAsync("api/Setores/Update-Setores", model);
+            var response = await setor.Content.ReadFromJsonAsync<Setores>();
+            return response!;
+        }
+    }
+}
